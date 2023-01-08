fe0e6d1cdd17824f01a6efba074b582e29302e5d
sivaprasad <sivaprasad.cs51@gmail.com>
2023-01-08 16:37:36 +0530

    added angular material

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+//let cars = ['tata'];
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-angular-app';
+
+  // constructor() {}
+  //ngOnInit(): void {}
+}
